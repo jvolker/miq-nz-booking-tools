31ac5b93e152bc63d911e411687814fdbc7b0f1e
Jeremias Volker <jeremias.volker@googlemail.com>
2021-07-12 09:19:44 +0200

    Update month

--- MIQ-Booking-Assistance/index.js
+++ MIQ-Booking-Assistance/index.js
@@ -2,7 +2,7 @@
 
 // CHANGE MONTH NUMBER TO DESIRED MONTH
 // january = 1, february = 2, march = 3, april = 4, may = 5, june = 6, july = 7, august = 8, september = 9, october = 10, november = 11, december = 12
-const month = 5;
+const month = 11;
 const accessibilityRequirement = false; // change to "true" if you have accessibility requirement
 
 // --------------
