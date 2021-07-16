92600f41d91242d46fbdea3ba4d54892d39a67eb
alexDrinkwater <alex.drinkwater@live.com>
2021-07-16 00:32:00 -0600

    Add the ability to search for specific days, months or any date.

--- MIQ-Booking-Assistance/renderer.js
+++ MIQ-Booking-Assistance/renderer.js
@@ -1,13 +1,22 @@
 const { ipcRenderer } = require('electron')
 
-const monthSelect = document.getElementById('months');
 const accessibilityRoomYes = document.getElementById('accessibilityRoom_yes');
 const accessibilityRoomNo = document.getElementById('accessibilityRoom_no');
+const findAnyDateYes = document.getElementById('findAnyDate_yes');
+const findAnyDateNo = document.getElementById('findAnyDate_no');
 const roomType = document.getElementById('roomType');
+const dateButton = document.getElementById('date-button');
+const monthButton = document.getElementById('month-button');
+const dateList = document.getElementById('date-list');
+const clearButton = document.getElementById('clear-button');
+const dateInput = document.getElementById('date-input');
+const dateDiv = document.getElementById('date-div');
+dateDiv.style.display = "none";
+
+let dates = [];
 
 let date = new Date();
 date.setMonth(date.getMonth() + 2);
-monthSelect.value = date.getMonth();
 sendSettings();
 
 ipcRenderer.on('status', function (evt, status) {
@@ -18,11 +27,58 @@ ipcRenderer.on('status-count', function (evt, status) {
     document.getElementById('status-count').innerText = status.message;
 });
 
-monthSelect.addEventListener('change', sendSettings)
 accessibilityRoomYes.addEventListener('change', sendSettings)
 accessibilityRoomNo.addEventListener('change', sendSettings)
 roomType.addEventListener('change', sendSettings)
 
+dateButton.addEventListener('click', () => {
+    if(dateInput.value && dates.indexOf(dateInput.value) === -1){
+        dates.push(dateInput.value);
+    }
+    updateDateUI();
+    sendSettings();
+})
+
+monthButton.addEventListener('click', () => {
+    if(dateInput.value && dates.indexOf(dateInput.value.substring(0, 7)) === -1){
+        dates.push(dateInput.value.substring(0, 7));
+    }
+    updateDateUI();
+    sendSettings();
+})
+
+clearButton.addEventListener('click', () => {
+    dates = [];
+    updateDateUI();
+    sendSettings();
+})
+
+findAnyDateYes.addEventListener('change', () => {
+    dateDiv.style.display = "none";
+    sendSettings();
+})
+
+findAnyDateNo.addEventListener('change', () => {
+    dateDiv.style.display = "";
+    sendSettings();
+})
+
+function updateDateUI() {
+    dateList.innerHTML = "";
+    dates.forEach(d => {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(d));
+        dateList.appendChild(li);
+    })
+}
+
 function sendSettings(){
-    ipcRenderer.send('settings', {month: monthSelect.value, accessibilityRequirement: accessibilityRoomYes.checked, roomType: roomType.value});
+    ipcRenderer.send(
+        'settings',
+        {
+            dates: dates,
+            accessibilityRequirement: accessibilityRoomYes.checked,
+            roomType: roomType.value,
+            findAnyDate: findAnyDateYes.checked
+        });
 }
